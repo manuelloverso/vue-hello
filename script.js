b1af29871ec8f93de56f4099b7683b5728eec6d7
manuelloverso <manuelloverso@gmail.com>
2024-03-11 11:49:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/* 
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: "Hello",
+      image: "https://picsum.photos/200/300",
+    };
+  },
+}).mount("#app");
